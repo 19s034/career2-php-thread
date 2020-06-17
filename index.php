6a641209372ff9e4862bed253b54e4d5f60e7511
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 09:43:57 +0900

    Bootstrap

--- index.php
+++ index.php
@@ -1,26 +1,42 @@
-<html>
-<head><title>掲示板App</title></head>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>掲示板App</title>
+</head>
 <body>
 
-<h1>掲示板App</h1>
+<div class="container">
+    <div class="col-md-8">
+        <h1 class="text-center text-primary py-3">掲示板App</h1>
 
-<h2>投稿フォーム</h2>
+        <h2 class="text-muted py-3">投稿フォーム</h2>
 
-<form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-<input type="text" name="personal_name" placeholder="名前" required><br><br>
-     <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
-</textarea><br><br>
-     <input type="submit" name="btn1" value="投稿する"/><br><br>
-</form>
+        <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+            <div class="form-group">
+                <label for="name">名前</label>
+                <input type="text" class="form-control" id="name" name="personal_name" placeholder="名前" required>
+            </div>
+            <div class="form-group">
+                <label for="contents">内容</label>
+                <textarea class="form-control" id="contents" name="contents" rows="8" cols="40" placeholder="内容" required></textarea>
+            </div>
+            <button class="btn btn-primary" type="submit">投稿する</button>
+        </form>
 
-<h2>スレッド</h2>
+        <hr>
 
-<form method="post" action="delete.php">
-    <button type="submit">投稿を全削除する</button>
-</form>
+        <h2 class="text-muted py-3">スレッド</h2>
+
+        <form method="post" action="delete.php">
+            <button class="btn btn-danger" type="submit">投稿を全削除する</button>
+        </form>
 
 <?php
-date_default_timezone_set("Asia/Tokyo");
+
 const THREAD_FILE = 'thread.txt';
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
@@ -37,8 +53,22 @@ function readData(){
         fclose($fp);
     }
 
-    $thread_text = file_get_contents(THREAD_FILE);
-    echo $thread_text;
+    $fp = fopen(THREAD_FILE, 'rb');
+
+    if ($fp){
+        if (flock($fp, LOCK_SH)){
+            while (!feof($fp)) {
+                $buffer = fgets($fp);
+                print($buffer);
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+        }
+    }
+
+    fclose($fp);
 }
 
 function writeData(){
@@ -49,8 +79,8 @@ function writeData(){
     $data = "<hr>\n";
     $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
     $data = $data."<p>投稿者: ".$personal_name."</p>\n";
-    $data = $data."<p>内容:".$contents."</p>\n";
-
+    $data = $data."<p>内容:</p>\n";
+    $data = $data."<p>".$contents."</p>\n";
 
     $fp = fopen(THREAD_FILE, 'ab');
 
@@ -67,13 +97,13 @@ function writeData(){
     }
 
     fclose($fp);
-
-    $redirect_url = $_SERVER['HTTP_REFERER'];
-    header("Location: $redirect_url");
-    exit;
-
 }
-
 ?>
+
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
